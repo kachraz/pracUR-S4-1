91272339c8f3ef2bf2ae79aa10e344b2913933a4
kachraz <kachraz@tuta.io>
2024-09-03 20:49:47 +0530

    addPantySmellis

--- src/CO/GaOv.jsx
+++ src/CO/GaOv.jsx
@@ -1,8 +1,10 @@
 // gfame over screen
+import upsa from "/x.webp";
 
 export default function GameOver({ winner }) {
   return (
     <div id="game-over">
+      <img src={upsa} alt="SnortHerPussyJuice" />
       <h2>FUCKED</h2>
       {winner && <p>{winner} SMELLPANTY</p>}
       {!winner && <p>😡DRAW😡</p>}
